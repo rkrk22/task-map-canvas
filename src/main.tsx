ccd53e09b84d4cd8f7512abc55864023521b3aa3
Ruslan Kim <squidcg@gmail.com>
2025-10-11 22:16:01 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import MessageBubble from "./components/hooks/MessageBubble"; // ← путь
+import MessageBubble from "./components/MessageBubble";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
