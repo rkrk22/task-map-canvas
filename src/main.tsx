79c1dc6bf78b81877be9cd97fdb8590bc7236d9b
Ruslan Kim <squidcg@gmail.com>
2025-10-11 22:07:06 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
+// src/main.tsx
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import MessageBubble from "./components/MessageBubble"; // ← добавь эту строку
+import MessageBubble from "./components/MessageBubble";
 
 createRoot(document.getElementById("root")!).render(
-  <>
+  <React.StrictMode>
     <App />
-    <MessageBubble url={import.meta.env.VITE_N8N_WEBHOOK_URL} /> {/* ← и это */}
-  </>
+    <MessageBubble url={import.meta.env.VITE_N8N_WEBHOOK_URL} />
+  </React.StrictMode>
 );
