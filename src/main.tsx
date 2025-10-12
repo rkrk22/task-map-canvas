0f1112c5dbb14fd191ccc5c011d3d31e3fcf7104
Ruslan Kim <squidcg@gmail.com>
2025-10-12 10:11:01 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,22 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-    <div style={{
-      position:"fixed", right:16, bottom:16, background:"#fff",
-      padding:12, borderRadius:12, boxShadow:"0 4px 20px rgba(0,0,0,.15)", zIndex:9999
-    }}>
+    <div
+      style={{
+        position: "fixed",
+        left: 16,
+        bottom: 16,
+        background: "#fff",
+        padding: 12,
+        borderRadius: 12,
+        boxShadow: "0 4px 20px rgba(0,0,0,.15)",
+        zIndex: 9999,
+      }}
+    >
       Тестовый бабл
     </div>
   </React.StrictMode>
