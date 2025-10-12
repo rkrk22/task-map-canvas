33a9c4d8946197f3c05e82b89a79b1411c739773
Ruslan Kim <squidcg@gmail.com>
2025-10-12 10:17:16 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
-// src/main.tsx
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+console.log("BUBBLE: mount start");
+
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-    <div className="fixed left-4 bottom-4 bg-white p-3 rounded-2xl shadow-lg z-50 rounded-2xl">
+    <div
+      data-testid="bubble"
+      className="fixed left-4 bottom-4 z-[999999] bg-white text-black p-3 rounded-2xl shadow-lg border border-black/10"
+    >
       Тестовый бабл
     </div>
   </React.StrictMode>
 );
+
+console.log("BUBBLE: rendered");
